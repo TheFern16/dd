377f42d192559edf9136a8c73936ca58168b118d
Matt <Matt@Homes-MacBook-Air.local>
2020-04-27 13:31:17 -0700

    Add handle change function

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,17 @@ import Board from '../components/board';
 export default function Index() {
   const boardSize = 7;
 
+  function handleChange() {
+    console.log('handleChange')
+  }
+
   return (
     <div className="main">
       <Post title="Game Board">
         <P>Select your board size</P>
         <P>Default is 8x8</P>
       </Post>
-      <input />
+      <input onChange={handleChange}/>
       <Board size={boardSize} />
     </div>
   )
