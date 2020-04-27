60ca8efc9f4ad6a3fdd907419d6291f7a8cf653a
Matt <Matt@Homes-MacBook-Air.local>
2020-04-27 12:59:33 -0700

    Add create matrix function

--- components/board.js
+++ components/board.js
@@ -1,6 +1,17 @@
-function createMatrix(size) {
+	// even is white
+	// odd is black
+function createMatrix(size, count = 0) {
+	var result = [];
+	for (var i = 0; i < size; i ++) {
+		let temp = [];
+		for (var j = 0; j < size; j++) {
+			if (j % 2 !== 0) temp.push(0)
+			if (j % 2 === 0) temp.push(1)
+		}
+		result.push(temp);
+	}
 	return (
-		<h1>{size}</h1>
+		<h1>{result}</h1>
 	)
 }
 
