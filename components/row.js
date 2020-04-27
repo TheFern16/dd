c9b4aab78bdaec18d20b943183e9830845970055
Matt <Matt@Homes-MacBook-Air.local>
2020-04-27 15:08:16 -0700

    Populate the bottom two rows with the correct colors

--- components/row.js
+++ components/row.js
@@ -2,26 +2,27 @@ function chooseStyle(number, key, arrLength) {
 	if (key < ((arrLength / 2) / 2) && number === 1) {
 		return 'box black red'
 	}
-	if (key >= arrLength - 2 && number === 1) {
-		return 'box black red';
+	if (key <= 1 && number === 0) {
+		return 'box white blackPiece';
 	}
 	if (number === 1) return 'box black';
 	if (number === 0) return 'box white';
 };
 
-function formatSquare(number) {
-	if (number === 1) {
-		return number
-	} else {
-		return '';
+function formatSquare(number, key, arrLength) {
+	if (key >= arrLength - 2 && number === 0) {
+		return '0';
 	}
+	if (number === 1) {
+		return '0'
+	} 
 }
 
 
 export default ({ row, i, arrayLength }) => (
 	<div className="boxContainer">{row.map(square => {
 			return(
-				<p className={chooseStyle(square, i, arrayLength)}>{formatSquare(square)}</p>
+				<p className={chooseStyle(square, i, arrayLength)}>{formatSquare(square, i, arrayLength)}</p>
 			)
 		})}
 		<style jsx> {`
@@ -43,6 +44,9 @@ export default ({ row, i, arrayLength }) => (
 			}
 			.red {
 				color: red;
+			}
+			.blackPiece {
+				color: black;
 			}
 		`}
 		</style>
