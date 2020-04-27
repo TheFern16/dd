731252e7e9f29db439fdc60ee02bbad588571e93
Matt <Matt@Homes-MacBook-Air.local>
2020-04-27 13:28:42 -0700

    Index is now stateful

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import P from '../components/paragraph'
 import Post from '../components/post'
 import Board from '../components/board';
 
-export default () => (
-  <div className="main">
-    <Post title="Game Board">
-      <P>Select your board size</P>
-      <P>Default is 8x8</P>
-    </Post>
-    <input />
-    <Board size="8"/>
-  </div>
-)
+export default function Index() {
+
+  return (
+    <div className="main">
+      <Post title="Game Board">
+        <P>Select your board size</P>
+        <P>Default is 8x8</P>
+      </Post>
+      <input />
+      <Board size="8"/>
+    </div>
+  )
+}
 
